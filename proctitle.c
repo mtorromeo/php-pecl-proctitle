05e7c3e368fae981f6132c1f2979ef9a42b56d78
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-01-28 23:01:53 +0100

    Added arginfo to the functions

--- proctitle.c
+++ proctitle.c
@@ -65,7 +65,11 @@ PHP_MINIT_FUNCTION(proctitle)
 }
 #endif
 
-/* {{{ proto void setproctitle(string title)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_title, 0, 0, 1)
+    ZEND_ARG_INFO(0, title)
+ZEND_END_ARG_INFO()
+
+/*
  * Changes the current process' title in system's list of processes
  */
 PHP_FUNCTION(setproctitle)
@@ -113,10 +117,10 @@ PHP_FUNCTION(setthreadtitle)
  *
  * Every user visible function must have an entry in proctitle_functions[].
  */
-static zend_function_entry proctitle_functions[] = {
-	PHP_FE(setproctitle,	NULL)
+static const zend_function_entry proctitle_functions[] = {
+	PHP_FE(setproctitle, arginfo_title)
 #if HAVE_PRCTL
-	PHP_FE(setthreadtitle,	NULL)
+	PHP_FE(setthreadtitle, arginfo_title)
 #endif
 	{NULL, NULL, NULL}	/* Must be the last line in proctitle_functions[] */
 };
